417cb2c0314cc2574b3732b4e90bea330972809a
MK87543 <134004067+MK87543@users.noreply.github.com>
2024-12-03 08:41:52 +0100

    02 JS Strings & Zahlen_DONE

---
+++ JavaScript_Basics/02 JS Strings_ Zahlen/script.js
@@ -0,0 +1,8 @@
+let a = 17.4;
+let b = 3.9;
+let result = 17.4 / 3.9;
+let firstname = "Matteo";
+let lastname = "Kleber";
+
+console.log("The result of " + a + " divided by " + b + " is " + result);
+console.log("ich heiße " + firstname.toUpperCase() + " " + lastname.toUpperCase());
