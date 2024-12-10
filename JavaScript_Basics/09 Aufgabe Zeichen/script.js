9d0bc3cc7f060249bbc5cfad07d57c5c3a8881fb
MK87543 <134004067+MK87543@users.noreply.github.com>
2024-12-10 09:25:24 +0100

    09 Aufgabe Zeichen_DONE

---
+++ JavaScript_Basics/09 Aufgabe Zeichen/script.js
@@ -0,0 +1,14 @@
+let row = 4;
+let collum = 4;
+
+for (let i = 0; i < row; i++) {
+
+    if (i % 2 == 0) {
+        console.log("XXXX");
+    }
+
+    else {
+        console.log("OOOO");
+
+    }
+}
