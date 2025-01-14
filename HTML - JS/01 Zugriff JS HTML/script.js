c862930406ffdf4acc6ded3329b8758f286c60ba
MK87543 <134004067+MK87543@users.noreply.github.com>
2025-01-14 09:14:31 +0100

    01_Zugriff_JS_HTML_DONE

--- HTML - JS/01 Zugriff JS HTML/script.js
+++ HTML - JS/01 Zugriff JS HTML/script.js
@@ -14,25 +14,35 @@ document.getElementById("button").addEventListener("click", function () {
     else {
         if (operator === '+') {
             const sum = parseFloat(value1) + parseFloat(value2)
-            document.getElementById("result").innerText = "Sum: " + sum;
+            document.getElementById("result").innerText = sum;
         }
 
         if (operator === '*') {
             const sum = parseFloat(value1) * parseFloat(value2)
-            document.getElementById("result").innerText = "Product: " + sum;
+            document.getElementById("result").innerText = sum;
         }
 
         if (operator === '-') {
             const sum = parseFloat(value1) - parseFloat(value2)
-            document.getElementById("result").innerText = "Sum: " + sum;
+            document.getElementById("result").innerText = sum;
         }
 
         if (operator === '/') {
             const sum = parseFloat(value1) / parseFloat(value2)
-            document.getElementById("result").innerText = "Sum: " + sum;
+            document.getElementById("result").innerText = sum.toFixed(5);
         }
 
     }
 
 });
 
+
+document.getElementById("clear").addEventListener("click", function () {
+
+    document.getElementById("value1").value = "";
+    document.getElementById("value2").value = "";
+    document.getElementById("op").value = "";
+    document.getElementById("result").innerText = "result";
+
+
+});
