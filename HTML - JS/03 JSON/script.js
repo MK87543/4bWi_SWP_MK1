9fbb2d095df0f2c5495c143a79d5d9bd62c979b6
MK87543 <134004067+MK87543@users.noreply.github.com>
2025-01-24 15:44:13 +0100

    03_JSON_DONE

--- HTML - JS/03 JSON/script.js
+++ HTML - JS/03 JSON/script.js
@@ -3,6 +3,8 @@ let data = [
     { firstname: "Paul", lastname: "Müller", point: 25 },
     { firstname: "Franze", lastname: "Maier", point: 10 },
     { firstname: "Geter", lastname: "Psteu", point: 35 },
+    { firstname: "Der", lastname: "Beste", point: 48 },
+
 
 
 ];
@@ -23,13 +25,40 @@ data.forEach(element =>{
 
 
 function loadPeople() {
-    let html = "";
+    /*   let html_F = "";
+       let html_L = "";
+       let html_P = "";
+   
+       data.forEach(element => {
+           html_F += "<div>" + element.firstname + "</div>"
+           html_L += "<div>" + element.lastname + "</div>"
+           html_P += "<div>" + element.point + "</div>"
+   
+   
+       });
+   
+       document.getElementById("firstname").innerHTML = html_F;
+       document.getElementById("lastname").innerHTML = html_L;
+       document.getElementById("points").innerHTML = html_P;
+   
+   
+   
+   }
+   
+   */
+
+    let tableBody = "";
+
     data.forEach(element => {
-        html += "<div>" + element.firstname + " " + element.lastname + "</div>"
+        tableBody += `
+           <tr>
+               <td>${element.firstname}</td>
+               <td>${element.lastname}</td>
+               <td>${element.point}</td>
+           </tr>`;
     });
 
-    document.getElementById("content").innerHTML = html;
+    document.getElementById("table-body").innerHTML = tableBody;
 
 }
-
 loadPeople();
