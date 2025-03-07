12fbdf79b5e630981ef589ca8e81641dba17d549
MK87543 <134004067+MK87543@users.noreply.github.com>
2025-03-07 16:38:46 +0100

    React_1st_commit_added_Liste

--- React/src/App.jsx
+++ React/src/App.jsx
@@ -2,26 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Card from './componentes/card.jsx'
-
+import Liste from './components/Liste.jsx'
 
 function App() {
-  const [count, setCounta] = useState(0)
+  const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1 className="bg-green-400 p-4 text-center">hello</h1>
-
-      <div className="flex">
-        <Card name={"Lenz"} jobtitle={"AV"} desciption={"lässig"} />
-        <Card name={"Sams"} jobtitle={"AV"} desciption={"auch lässig"} />
-
-      </div>
-
-
-
-
-      test
+      <Liste />
 
     </>
   )
---
+++ React/src/components/Liste.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Liste() {
+    return (
+        <div className="border border-gray-400 divide-y divide-gray-400 rounded-md ">
+            <li>Coffe</li>
+            <li>Tea</li>
+            <li>Beer</li>
+
+        </div>
+    )
+}
