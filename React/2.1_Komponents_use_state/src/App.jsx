4c5374ebb484a98a4292ac40b94e38589bb1fab5
MK87543 <134004067+MK87543@users.noreply.github.com>
2025-03-18 08:54:25 +0100

    React_2nd_commit_2.1_Komponent_state_DONE

--- React/2.1_Komponents_use_state/src/App.jsx
+++ React/2.1_Komponents_use_state/src/App.jsx
@@ -2,53 +2,18 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Liste from './components/Liste.jsx'
-import Button from './components/Button.jsx'
-import Card from './components/Card.jsx'
+import Clicks from './components/click_count.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div className='grid grid-cols-3 gap-4'>
-        <Liste l1="coffe" l2="cola" l3="tea" />
-        <Liste l1="tea" l2="coffee" l3="tea" />
-        <Liste l1="fanta" l2="bear" l3="water" />
-        <Liste l1="coffe" l2="cola" l3="coffee" />
-        <Liste l1="tea" l2="cola" l3="tea" />
-        <Liste l1="coffee" l2="cola" l3="cola" />
-      </div>
-
-
-      <div className='flex justify-around'>
-        <Button info="clear" />
-        <Button info="try it" />
-        <Button info="big button" />
-      </div>
-
-      <div className=' grid grid-cols-3 gap-4'>
-        <Card Name="John Doe" info="Software Engineer" />
-        <Card Name="Jane Smith" info="UX Designer" />
-        <Card Name="Peter Gsteu" info="Bester Mann" />
-        <Card Name="Matthias Hagen" info="besser als Peta" />
-        <Card Name="Terry Cruse" info="Inspiration" />
-        <Card Name="David Goggins" info="hardest man on earth" />
-        <Card Name="Raphi" info="isch do" />
-        <Card Name="Elina" info="confident" />
-        <Card Name="geter magen" info="besser als beide" />
-        <Card Name="hatthias msteu" info="besser als der oane" />
-
-
-      </div>
-
-      <div>
-        <Button info="be friendly" />
-
-      </div>
-
-
-    </>
+    <div className='grid grid-cols-3 gap-4'>
+      <Clicks />
+      <Clicks />
+      <Clicks />
+      <Clicks />
+    </div>
   )
 }
 
--- React/2.1_Komponents_use_state/src/components/click_count.jsx
+++ React/2.1_Komponents_use_state/src/components/click_count.jsx
@@ -1,7 +1,18 @@
-import React from 'react'
+import React, { useState } from "react";
 
-export default function click_count() {
+export default function Clicks({ }) {
+    const [amountOfClicks, setAmountOfClicks] = useState(0);
+
+    let style = amountOfClicks > 5 ? "bg-green-400" : "";
+    let style2 = amountOfClicks > 50 ? "bg-green-900" : "";
+
     return (
-        <div>click_count</div>
-    )
+        <div className={`border p-4 hover:cursor-pointer ${style}  ${style2}`} onClick={() => {
+            setAmountOfClicks(amountOfClicks + 1)
+        }}>
+            <h1 className="font-extrabold text-2xl">Hello User !</h1>
+            <p>Click me!</p>
+            <div>Amount of Clicks: {amountOfClicks}</div>
+        </div>
+    );
 }
