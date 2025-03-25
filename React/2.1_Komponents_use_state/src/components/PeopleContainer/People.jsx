b82e73a88a4df1ef76e87c2d2c5000bbda988cd5
MK87543 <134004067+MK87543@users.noreply.github.com>
2025-03-25 08:48:57 +0100

    React_3_03_commit_added_useState_button

--- React/2.1_Komponents_use_state/src/components/PeopleContainer/People.jsx
+++ React/2.1_Komponents_use_state/src/components/PeopleContainer/People.jsx
@@ -3,6 +3,7 @@ import P_Card from "./P_Card";
 
 export default function People() {
   const [people, setPeople] = useState([]);
+  const [color, setColor] = useState("green");
 
   useEffect(() => {
     fetch("https://67dd7d32e00db03c406bd96e.mockapi.io/person")
@@ -12,10 +13,15 @@ export default function People() {
       });
   }, []);
 
+  useEffect(() => {
+    alert("change")
+  }, [color]);
+
   return (
     <div>
-    <h1 className="font-bold text-3xl w-full flex justify-center items-center mb-8">People</h1>
-          <div className="grid grid-cols-2 lg:grid-cols-4 gap-4">
+      <h1 className="font-bold text-3xl w-full flex justify-center items-center mb-8">People</h1>
+      <button onClick={() => { setColor("orange") }}>change color</button>
+      <div className="grid grid-cols-2 lg:grid-cols-4 gap-4">
         {people.map((person) => (
           <P_Card
             key={person.id}
