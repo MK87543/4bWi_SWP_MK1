fdda84b9501f4ddb58620f96d1b5dca5cebdf3b9
MK87543 <134004067+MK87543@users.noreply.github.com>
2025-03-28 16:11:14 +0100

    React_3_07_commit_added_Router_pass_data

--- React/2.1_Komponents_use_state/src/components/pages/Page1.jsx
+++ React/2.1_Komponents_use_state/src/components/pages/Page1.jsx
@@ -2,10 +2,13 @@ import React from 'react'
 import { Link } from 'react-router'
 
 export default function Page1() {
+  const person = { firstname: "hans", lastname: "müller" }
+
   return (
+
     <>
       <h1 className='p-8'>Page1</h1>
-      <Link to="/page2" className='bg-green-400 rounded-2xl hover:cursor-pointer hover:bg-green-900 p-4'>Dashboard</Link>
+      <Link to="/page2" state={{ person: person, title: "test" }} className='bg-green-400 rounded-2xl hover:cursor-pointer hover:bg-green-900 p-4'>Dashboard</Link>
     </>
   );
 }
--- React/2.1_Komponents_use_state/src/components/pages/Page2.jsx
+++ React/2.1_Komponents_use_state/src/components/pages/Page2.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
-import { Link } from 'react-router'
+import { Link, useLocation } from 'react-router'
 
 export default function Page1() {
+  const location = useLocation();
+  const person = location.state.person;
   return (
     <>
       <h1>Page2</h1>
+      <div>Hello {person.firstname}</div>
       <Link to="/" className='hover:cursor-pointer'>Link to: Page1</Link>
     </>
   );
