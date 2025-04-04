ff5fea53fd1af8aba973e390259be1cd89b3a86f
MK87543 <134004067+MK87543@users.noreply.github.com>
2025-04-04 15:38:03 +0200

    React_3_08_commit_added_changed_font

--- React/2.1_Komponents_use_state/src/components/pages/Page1.jsx
+++ React/2.1_Komponents_use_state/src/components/pages/Page1.jsx
@@ -8,6 +8,7 @@ export default function Page1() {
 
     <>
       <h1 className='p-8'>Page1</h1>
+      <p className='p-2 font-main'>new font</p>
       <Link to="/page2" state={{ person: person, title: "test" }} className='bg-green-400 rounded-2xl hover:cursor-pointer hover:bg-green-900 p-4'>Dashboard</Link>
     </>
   );
